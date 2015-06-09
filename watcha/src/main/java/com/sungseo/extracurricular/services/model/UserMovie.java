8b1485fdced29f2a86c399c121caad945b5c064d
wooklym <wooklym@gmail.com>
2015-06-09 18:16:19 +0900

    watcha
    왓챠 관심상품 완

--- watcha/src/main/java/com/sungseo/extracurricular/services/model/UserMovie.java
+++ watcha/src/main/java/com/sungseo/extracurricular/services/model/UserMovie.java
@@ -1,6 +1,5 @@
 package com.sungseo.extracurricular.services.model;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
